443748cf5cdc4245b4db9baa1027bd7bd6503a0a
jbock <jbock-java@gmx.de>
2025-06-08 18:55:31 +0200

    read password from stdin if needed

--- login.c
+++ login.c
@@ -1,19 +1,15 @@
-/* Andrew Morgan (morgan@parc.power.net) -- a self contained `blank'
- * application
- *
- * I am not very proud of this code.  It makes use of a possibly ill-
- * defined pamh pointer to call pam_strerror() with.  The reason that
- * I was sloppy with this is historical (pam_strerror, prior to 0.59,
- * did not require a pamh argument) and if this program is used as a
- * model for anything, I should wish that you will take this error into
- * account.
- */
+/* starting point: linux-pam examples/blank.c */
 
 #include <stdio.h>
 #include <pwd.h>
+#include <unistd.h>
+#include <string.h>
+#include <termios.h>
 
 #include <security/pam_appl.h>
 #include <security/pam_misc.h>
+
+#define INPUTSIZE 4096 /* maximum length of input+1 */
 
 static int bail_out(pam_handle_t *pamh, int code, const char *fn) {
   fprintf(stderr, "==> called %s()\n  got: `%s'\n", fn,
@@ -22,11 +18,42 @@ static int bail_out(pam_handle_t *pamh, int code, const char *fn) {
   return 1;
 }
 
+void read_string(char **retstr) {
+  char line[INPUTSIZE];
+  /* set echo off */
+  struct termios term_before, term_tmp;
+  tcgetattr(STDIN_FILENO, &term_before);
+  memcpy(&term_tmp, &term_before, sizeof(term_tmp));
+  term_tmp.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_tmp);
+  /* read password */
+  int nc = read(STDIN_FILENO, line, INPUTSIZE - 1);
+  /* set echo on */
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_before);
+  if (nc <= 0) {
+    *retstr = NULL;
+  } else {
+    if (line[nc - 1] == '\n') {     /* <NUL> terminate */
+      line[nc - 1] = '\0';
+    }
+    *retstr = strdup(line);
+  }
+}
+
 int my_conv(
     int num_msg,
     const struct pam_message **msgm,
     struct pam_response **response,
     void *appdata_ptr) {
+  char *password;
+  if (appdata_ptr == NULL) {
+    fprintf(stdout, "Password: ");
+    fflush(stdout);
+    read_string(&password);
+    fprintf(stdout, "\n");
+  } else {
+    password = (char*) appdata_ptr;
+  }
   if (num_msg <= 0) {
     return PAM_CONV_ERR;
   }
@@ -42,7 +69,7 @@ int my_conv(
       case PAM_PROMPT_ECHO_OFF:
       case PAM_PROMPT_ECHO_ON:
         reply[count].resp_retcode = 0;
-        reply[count].resp = (char*) appdata_ptr;
+        reply[count].resp = password;
         break;
       case PAM_ERROR_MSG:
         fprintf(stderr, "PAM_ERROR: %s\n", msgm[count]->msg);
@@ -61,17 +88,21 @@ int my_conv(
 
 int main(int argc, char **argv) {
   pam_handle_t *pamh = NULL;
-  char *username = NULL;
+  char *username;
+  char* password;
   int retcode;
 
   /* did the user call with a username as an argument ? */
-  if (argc != 3) {
-    fprintf(stderr, "usage: %s [username] [password]\n", argv[0]);
+  if (argc == 2) {
+    password = NULL;
+  } else if (argc == 3) {
+    password = (char*) malloc(strlen(argv[2]) * sizeof(char));
+    strcpy(password, argv[2]);
+  } else {
+    fprintf(stderr, "usage: %s username [password]\n", argv[0]);
     return 1;
   }
   username = argv[1];
-  char* password = (char*) malloc(strlen(argv[2]) * sizeof(char));
-  strcpy(password, argv[2]);
   struct pam_conv conv = {
     my_conv,
     password /* this is the `appdata_ptr' */
@@ -96,11 +127,6 @@ int main(int argc, char **argv) {
     return bail_out(pamh, retcode, "pam_setcred1");
   }
 
-  retcode = pam_acct_mgmt(pamh, 0);
-  if (retcode != PAM_SUCCESS) {
-    return bail_out(pamh, retcode, "pam_acct_mgmt");
-  }
-
   const void *item = NULL;
   retcode = pam_get_item(pamh, PAM_USER, &item);
   if (retcode != PAM_SUCCESS) {
@@ -113,20 +139,6 @@ int main(int argc, char **argv) {
   fprintf(stdout, "gid:       %d\n", pwd->pw_gid);
   fprintf(stdout, "homedir:   %s\n", pwd->pw_dir);
   fprintf(stdout, "shell:     %s\n", pwd->pw_shell);
-
-  /* open a session for the user --- `0' could be PAM_SILENT */
-  retcode = pam_open_session(pamh, 0);
-  if (retcode != PAM_SUCCESS) {
-    return bail_out(pamh, retcode, "pam_open_session");
-  }
-
-  fprintf(stdout, "The user has been authenticated and `logged in'\n");
-
-  /* close a session for the user --- `0' could be PAM_SILENT */
-  retcode = pam_close_session(pamh, 0);
-  if (retcode != PAM_SUCCESS) {
-    return bail_out(pamh, retcode, "pam_close_session");
-  }
 
   retcode = pam_setcred(pamh, PAM_DELETE_CRED);
   if (retcode != PAM_SUCCESS) {
